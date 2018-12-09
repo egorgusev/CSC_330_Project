81f683aeccce076680b5a404d20ffecb63f3c96f
Egor <yegor_23@yahoo.com>
2018-12-08 20:14:15 -0500

    ver 1.2

---
+++ src/main/java/Interfaces/FridgeSections.java
@@ -0,0 +1,10 @@
+package FridgeSections;
+
+public abstract interface FridgeSections {
+    abstract getInfo();
+    abstract temperatureSetting;
+    abstract limit;
+    abstract
+
+
+}
---
+++ src/main/java/Interfaces/Thermostat.java
@@ -0,0 +1,6 @@
+package Interfaces.Thermostat;
+
+public interface Thermostat {
+    Double temperature = 0.0;
+    Double checkTemperature();
+}
